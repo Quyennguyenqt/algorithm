83071b42eab0ee7a3d881e81519ddd843a9aa2cc
quyen nguyen <nguyenquyen0201qt@gmail.com>
2021-12-25 13:57:31 +0700

    commit

--- src/com/nqt/Demo.java
+++ src/com/nqt/Demo.java
@@ -3,5 +3,7 @@ package com.nqt;
 public class Demo {
     public static void main(String[] args) {
         System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
